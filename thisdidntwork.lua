3f0f3171e15097c0a600339d5ae6d611d6c046a6
Balogina <67536698+bumpkinfanboi@users.noreply.github.com>
2025-12-15 21:40:26 +0000

    theorizing in thisdidntwork

--- thisdidntwork.lua
+++ thisdidntwork.lua
@@ -3,3 +3,5 @@ local number
 input = io.read()
 number = tonumber(string.match(input, "%d+"))
 print(number) 
+for i=1,mapandnumofdoor_[2] do
+    if PlayerPosition[1] == DoorPositions[PlayerPosition[3]][i][2] and PlayerPosition == DoorPositions[PlayerPosition[3]][i][3] then
